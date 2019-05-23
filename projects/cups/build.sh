e96c90966266f7a1f441c97824d3b507472352f4
Sean Kau <skau@chromium.org>
2019-05-23 14:44:33 -0700

    strip off libraries we do not need

--- projects/cups/build.sh
+++ projects/cups/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-./configure --enable-static --disable-shared --enable-sanitizer --disable-libusb --with-components=libcupslite
+./configure --enable-static --disable-gnutls --disable-shared --enable-sanitizer --disable-libusb --with-components=libcupslite
 make clean
 make install-libs
+
+$CXX $CXXFLAGS -std=c++11 -I. \
+    $SRC/fuzz/cups_ppdopen_fuzzer.cc -o $OUT/cups_ppdopen_fuzzer \
+    $LIB_FUZZING_ENGINE /usr/lib/libcups.a
