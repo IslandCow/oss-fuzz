6c0787ee97cac7f90f398d081b30a62df3a7a8d2
Sean Kau <skau@chromium.org>
2019-05-23 14:54:58 -0700

    2.2 does not have libcupslite

--- projects/cups/build.sh
+++ projects/cups/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./configure --enable-static --disable-gnutls --disable-shared --enable-sanitizer --disable-libusb --with-components=libcupslite
+./configure --enable-static --disable-gnutls --disable-shared \
+   --disable-libusb --with-components=core
+
 make clean
 make install-libs
 
